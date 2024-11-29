5cfb8d0ac6d52e638eb31a9f83921a17f6c89650
B00150046 <145692695+B00150046@users.noreply.github.com>
2024-11-29 14:23:56 +0000

    S

--- doughnuts/src/app/manager/page.js
+++ doughnuts/src/app/manager/page.js
@@ -1,5 +1,6 @@
 'use client';
-
+import Link from 'next/link'
+ 
 import { useState, useEffect } from "react";
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
@@ -10,13 +11,26 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import TextField from '@mui/material/TextField';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Container, Icon, Table, TableContainer, TableHead } from '@mui/material';
 
 
 export default function DoughnutApp() {
+const [data, setData] = useState([])
+const [pro, setProducts] = useState([])
+const [cart, setCart] = useState([])
 const [order, setOrders] = useState([])
 const [weather, setWeatherData] = useState(0)
 
+
+
+const newCartItem = (pname, cost) => {
+    const url = `http://localhost:3000/api/newCartItem?pname=${pname}&price=${cost}`;
+    
+    console.log("Handling submit for:", url);
+    runDBCallAsync(url);
+    alert("Item added to cart");
+};
 
   async function runDBCallAsync(url) {
     const res = await fetch(url);
@@ -26,11 +40,35 @@ const [weather, setWeatherData] = useState(0)
 
 useEffect(() => {
 
+
+        
+    fetch('http://localhost:3000/api/getData')
+    .then((data) => data.json())
+    .then((data) => {
+       
+        console.log("session status" + data.data)
+
+        if(data.data == false){
+            window.location = '/newregister'
+        }
+    })
     
     fetch('http://localhost:3000/api/getWeather')
     .then((weather) => weather.json())
     .then((weather) => {
         setWeatherData(weather)
+    })
+
+        fetch('http://localhost:3000/api/getDoughnuts')
+        .then((pro )=> pro.json())
+        .then((pro) => {
+        setProducts(pro)
+    })
+    
+    fetch('http://localhost:3000/api/ShoppingCart')
+    .then((cart) => cart.json())
+    .then((cart) => {
+        setCart(cart)
     })
     
     fetch('http://localhost:3000/api/getOrders')
@@ -43,14 +81,39 @@ useEffect(() => {
 
  //____________________________________________________________________________________
 //PAGES FOR MULTI-PAGE APP
-   
-    const [showProfile, setShowProfile] = useState(true);
-   
+   const [showHome, setShowHome] = useState(true);
+    const [showCart, setShowCart] = useState(false);
+    const [showProducts, setShowProducts] = useState(false);
+    const [showCheckOut, setShowCheckout] = useState(false);
+
+    
+    function handleHome() {
+        setShowHome(true);
+        setShowCart(false);
+        setShowProducts(false);
+        setShowCheckout(false);
+    }
+
+    function handleCart() {
+        setShowHome(false);
+        setShowCart(true);
+        setShowProducts(false);
+        setShowCheckout(false)
+    }
+    function handleProducts() {
+        setShowHome(false);
+        setShowCart(false);
+        setShowProducts(true);
+        setShowCheckout(false)
+    }
+    function handleCheckOut() {
+        setShowRegister(false);
+        setShowCart(false);
+        setShowProducts(false);
+        setShowCheckout(true)
+    } 
 
     //____________________________________________________________________________________
-
-   
-    
     //if(!cart) return <p>No cart items sorry! </p>
    // if(!order) return <p>Loading</p>
    // if(!weather) return <p>No weather</p>
@@ -78,7 +141,22 @@ useEffect(() => {
                             sx={{ flexGrow: 1, textAlign: 'center' }}
                         />
                     </Typography>
-                   <div>
+                    <Button
+                        onClick={handleProducts}
+                        sx ={{
+                            color: '#355746',
+                            //make font bold
+                            fontWeight: 'bold',
+                            '&:hover': {
+                                color: '#fff',
+                            },
+                        }}>
+                        Products
+                    </Button>
+                    
+                    <div sx={{
+                        size: 'small'
+                    }}>  
                     Today's temperature: {JSON.stringify(weather.temp)}
                     </div>
                 </Toolbar>
@@ -86,14 +164,137 @@ useEffect(() => {
             <Container component="main" maxWidth="xs">
                
             </Container>
-            {showProfile && (
-                <Box>
-                    <h1>Profile</h1>
-                    <Button onClick={() => setShowProfile(false)}>Go to Orders</Button>
-                    
+            {showHome && (
+                <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                    <Typography variant="h4" sx={{ textAlign: 'center' }}>
+                        Welcome to Doughnuts
+                    </Typography>
+                    <Typography variant="body1" sx={{ textAlign: 'center' }}>
+                        AAAAAAAAAAAAAAAAAAAAAAA
+                    </Typography>
                 </Box>
             )}
-           
+
+
+            {showCart && (
+                <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                   <div>
+                          <h2>Shopping Cart</h2>
+                          <TableContainer>
+                            <Table>
+                                 <TableHead>
+                                      <tr>
+                                        <th>Item</th>
+                                        <th>Price</th>
+                                      </tr>
+                                 </TableHead>
+                                 <tbody>
+                                      {cart.map((item, i) => (
+                                        <tr key={i}>
+                                             <td>{item.item_name}</td>
+                                             <td>{item.price}</td>
+                                        </tr>
+                                      ))}
+                                 </tbody>
+                            </Table>
+                          </TableContainer>
+                   </div>
+
+                   <Link href="/CheckOut">Checkout</Link>
+                </Box>
+            )}
+            
+            {showProducts && (
+                
+                <Box component="section" sx={{ p: 2, }}>
+                    <Typography variant="h4" sx={{ textAlign: 'center' }}>
+                   
+                        Our Products
+                        </Typography>
+                    <div
+                    sx = {{
+                    //Display in a grid
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(3, 1fr)',
+                    gap: '20px',
+
+                    
+                    }}>
+                    {
+                     pro.map((item, i) => (
+                     <div style={{padding: '20px',
+                        margin: '20px',
+                        border: '3px solid #cd0f2a',
+                        background: '#b7edd4',
+                        alignContent: 'center',
+                        borderRadius: '5px',
+                        width: '175px',
+                     }} key={i} >
+                     <img src={item.img_src} alt={item.p_name} width={100} height={100} />
+                    <br></br>
+                    <div sx={{
+                        alignContent: 'center',
+                        fontFamily: 'Arial',
+                        fontSize: '1.5em',
+                        fontWeight: 'bold',
+                        color: '#355746',
+                    }}>
+                    {item.p_name}
+                    
+                     
+                    € {item.price}
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <Button 
+                    onClick={() => newCartItem(item.p_name, item.price)} 
+                    sx={{
+                        backgroundColor: '#cd0f2a',
+                        color: '#fff',
+                        '&:hover': {
+                            backgroundColor: '#fff',
+                            color: '#cd0f2a',
+                        },
+                    }}
+                    > Add to cart </Button>
+                    </div>
+                    ))
+                    }
+                    <Button
+                        onClick={handleCheckOut}
+                        sx ={{
+                            backgroundColor: '#cd0f2a',
+                            color: '#fff',
+                            '&:hover': {
+                                backgroundColor: '#fff',
+                                color: '#cd0f2a',
+                            },
+                        }}>
+                        Checkout
+                    </Button>
+                   <IconButton
+                    onClick={handleCart}
+                    sx = {{
+                        position: 'fixed',
+                        bottom: '20px',
+                        right: '20px',
+                        backgroundColor: '#cd0f2a',
+                        color: '#fff',
+                        '&:hover': {
+                            backgroundColor: '#fff',
+                            color: '#cd0f2a',
+                        },
+                    }}>
+                          <ShoppingCartIcon />
+                   </IconButton>
+                    </div>
+                </Box>
+            )}
+            {showCheckOut && (
+                <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                    This could be the checkout page!
+                </Box>
+            )}
         </Box>
     );
 }
