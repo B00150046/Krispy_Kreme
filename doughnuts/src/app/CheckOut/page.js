c3a558ab71f22f0672d0498ac67b2148396dd63c
B00150046 <145692695+B00150046@users.noreply.github.com>
2024-12-01 12:40:53 +0000

    Update page.js

--- doughnuts/src/app/CheckOut/page.js
+++ doughnuts/src/app/CheckOut/page.js
@@ -1,3 +1,19 @@
+'use client';
+import Link from 'next/link'
+ 
+import { useState, useEffect } from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import TextField from '@mui/material/TextField';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Container, Icon, Table, TableContainer, TableHead } from '@mui/material';
+
 import { useState, useEffect } from "react";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button, Typography, Box } from '@mui/material';
 
