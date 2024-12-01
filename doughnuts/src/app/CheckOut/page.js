43cb658d05f715d05713f062c80f3e9eb4a0aae4
B00150046 <145692695+B00150046@users.noreply.github.com>
2024-12-01 15:27:54 +0000

    Update page.js

--- doughnuts/src/app/CheckOut/page.js
+++ doughnuts/src/app/CheckOut/page.js
@@ -1,7 +1,5 @@
 'use client';
-import Link from 'next/link'
- 
-import { useState, useEffect } from "react";
+
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
