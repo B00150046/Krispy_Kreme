9c0be6c8a4630341ed9b693de368366226ebfb01
B00150046 <145692695+B00150046@users.noreply.github.com>
2024-12-01 20:43:58 +0000

    Update page.js

--- doughnuts/src/app/dashboard/page.js
+++ doughnuts/src/app/dashboard/page.js
@@ -29,7 +29,7 @@ export default function DoughnutApp() {
     };
 
     const deleteCartItem = async (pname, cost, timer) => {
-        const stringDate = timer.toLocaleString();
+        const stringDate = timer.toISOString();
         const url = `/api/deleteCartItem?item_name=${pname}&price=${cost}&time_added=${encodeURIComponent(stringDate)}`;
         console.log("Handling submit for:", url);
         const result = await runDBCallAsync(url);
